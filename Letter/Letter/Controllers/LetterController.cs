794b24c5b1f44d79eb60619ba0a034b8b108e8d6
una <amy58328@gmail.com>
2019-08-28 17:19:52 +0800

    reset complete

---
+++ Letter/Letter/Controllers/LetterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Letter.Controllers
+{
+    public class LetterController : Controller
+    {
+        // GET: Word
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+        public ActionResult Grade()
+        {
+            return View();
+        }
+
+        public ActionResult Answer()
+        {
+            return View();
+        }
+    }
+}
